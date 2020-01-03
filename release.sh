da7a9337ca5c3aba90ab47be756ad00f780efac3
nytopop <ericizoita@gmail.com>
2020-01-03 15:58:00 -0800

    check for pre-release in release.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -eux -o pipefail
 
-gothub release -u nytopop -r askii -t ${TAG} -d "$(< ${CHANGELOG})"
+if [[ ${TAG} =~ "pre" ]]; then
+	gothub release -p -u nytopop -r askii -t ${TAG} -d "$(< ${CHANGELOG})"
+else
+	gothub release -u nytopop -r askii -t ${TAG} -d "$(< ${CHANGELOG})"
+fi
